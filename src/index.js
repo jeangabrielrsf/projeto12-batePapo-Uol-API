039ce4c2a5b3a54af64c58f26755c5cfc6934efd
Jean Frusca <jeang96@gmail.com>
2022-09-01 13:43:03 -0300

    FEAT: conexão com o MongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+mongoClient.connect(() => {
+	db = mongoClient.db("bate_papo");
+});
 
 app.listen(5000, () => console.log("Listening on port 5000..."));
