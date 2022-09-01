1463d12dbb5c030c76e3d8fbd12cc8e84d269a0e
Jean Frusca <jeang96@gmail.com>
2022-09-01 14:09:22 -0300

    FEAT: criação das requisições de GET e POST

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
@@ -12,6 +12,53 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
 	db = mongoClient.db("bate_papo");
+});
+
+app.post("/participants", async (request, response) => {
+	try {
+		const { name } = request.body;
+		//FALTA VALIDAR COM A BIBLIOTECA JOY
+		//FALTA VERIFICAR SE JÁ TEM UM NOME EXISTENTE
+		const result = await db.collection("users").insertOne({
+			name,
+			lastStatus: Date.now,
+		});
+
+		return response.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		return response.sendStatus(500);
+	}
+});
+
+app.post("/messages", async (request, response) => {
+	try {
+		const { to, text, type } = request.body;
+		const from = request.headers.user;
+		return response.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		return response.sendStatus(500);
+	}
+});
+
+app.get("/messages", async (request, response) => {
+	try {
+		const messages = await db.collection("messages").find().toArray();
+		return response.send(messages);
+	} catch (error) {
+		console.log(error);
+		return response.sendStatus(500);
+	}
+});
+
+app.post("/status", async (request, response) => {
+	try {
+		const participant = request.headers.user;
+	} catch (error) {
+		console.log(error);
+		return response.sendStatus(500);
+	}
 });
 
 app.listen(5000, () => console.log("Listening on port 5000..."));
