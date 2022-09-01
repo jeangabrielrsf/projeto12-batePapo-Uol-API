6b134ab59cfd30f2415c8574767a19009e71d9b3
Jean Frusca <jeang96@gmail.com>
2022-09-01 13:45:37 -0300

    FEAT: utilizando a biblioteca dotenv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
 	db = mongoClient.db("bate_papo");
